c9c2d7d27c32562a18ab940fc504ab81b3231b77
Joseph <josephylee72@gmail.com>
2023-10-24 00:48:44 -0700

    completed attributes and properties

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,12 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name='Bobby Hill', breed='Mastiff'):
+        if type(name) == str and 1 <= len(name) <= 25:
+            self.name = name
+        else:
+            print('Name must be string between 1 and 25 characters.')
+        if breed in APPROVED_BREEDS:
+            self.breed = breed
+        else:
+            print('Breed must be in list of approved breeds.')
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,13 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name='Bobby Hill', job='Admin'):
+        if type(name) == str and 1 <= len(name) <= 25:
+            name = name.title()
+            self.name = name
+        else:
+            print('Name must be string between 1 and 25 characters.')
+        if job in APPROVED_JOBS:
+            self.job = job
+        else:
+            print('Job must be in list of approved jobs.')
